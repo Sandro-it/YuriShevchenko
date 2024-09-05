59261ea8f8eb270fc7dbcf54e9af3f8ee0c38a62
Sandro-it <kozachenkooleksandr@gmail.com>
2024-09-06 00:06:25 +0300

    BP1

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -62,6 +62,83 @@
 
 // export default WorkPage;
 
+// import { Helmet } from "react-helmet-async";
+// import { useParams, Link } from "react-router-dom";
+// import { useTranslation } from "react-i18next";
+// import styles from "../styles/WorkPage.module.css";
+// import worksData from "../data/worksData";
+
+// const WorkPage = () => {
+//   const { t } = useTranslation();
+//   const { workId } = useParams();
+//   const data = worksData();
+//   const work = data.find((work) => work.id === workId);
+
+//   if (!work) {
+//     return <div>{t("work_not_found")}</div>;
+//   }
+
+//   return (
+//     <>
+//       <Helmet>
+//         <title>{`Твір: ${work.title} | Work: ${work.title} - Yuri Shevchenko`}</title>
+//         <meta
+//           name="description"
+//           content={`Опис твору ${work.title}, створеного Юрієм Шевченком.
+//           Description of the work ${work.title} by Yuri Shevchenko.`}
+//         />
+//       </Helmet>
+//       <div className="container">
+//         <div className={styles.workPage}>
+//           <div className={styles.topSection}>
+//             <Link to="/legacy" className={styles.backButton}>
+//               {t("back_to_works")}
+//             </Link>
+//           </div>
+//           <div className={styles.contentSection}>
+//             <div className={styles.textSection}>
+//               <h1>{work.title}</h1>
+//               <p className={styles.description}>{work.description}</p>
+
+//               {/* Якщо є список, відображаємо його */}
+//               {work.list && work.list.length > 0 && (
+//                 <ol>
+//                   {work.list.map((item, index) => (
+//                     <li key={index}>{item}</li>
+//                   ))}
+//                 </ol>
+//               )}
+
+//               {/* Відображення відео посилання, якщо доступно */}
+//               {work.videoUrl && (
+//                 <p>
+//                   <a
+//                     className={styles.link}
+//                     href={work.videoUrl}
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                   >
+//                     {t("view_on_youtube")}
+//                   </a>
+//                 </p>
+//               )}
+//             </div>
+//             <div className={styles.imageSection}>
+//               <img
+//                 className={styles.photo}
+//                 src={work.imageUrl}
+//                 alt={work.title}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default WorkPage;
+
 import { Helmet } from "react-helmet-async";
 import { useParams, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -109,18 +186,22 @@ const WorkPage = () => {
                 </ol>
               )}
 
-              {/* Відображення відео посилання, якщо доступно */}
-              {work.videoUrl && (
-                <p>
-                  <a
-                    className={styles.link}
-                    href={work.videoUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {t("view_on_youtube")}
-                  </a>
-                </p>
+              {/* Відображення кількох відео-посилань, якщо доступні */}
+              {work.videoUrls && work.videoUrls.length > 0 && (
+                <div>
+                  {work.videoUrls.map((url, index) => (
+                    <p key={index}>
+                      <a
+                        className={styles.link}
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {`${t("view_on_youtube")} ${index + 1}`}
+                      </a>
+                    </p>
+                  ))}
+                </div>
               )}
             </div>
             <div className={styles.imageSection}>
